4690dd83b933e6c01b3f44ad39a6b5eced80c2ab
Justin Grant <jlgrock@hotmail.com>
2014-12-31 13:46:33 -0500

    removing newly unecessary files

--- docker/sh/init.sh
+++
@@ -1,9 +0,0 @@
-echo "Starting mongodb..."
-
-# Set monitoring mode to allow for job control
-set -m
-
-mongod &
-mongo --nodb /bootstrap/sh/testConnection.js
-mongo /bootstrap/data/test-data.js
-fg
