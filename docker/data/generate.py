13bca65abfc3e5ad2356b747f78835aa3ea668f6
Justin Grant <jlgrock@hotmail.com>
2015-01-06 15:30:10 -0500

    starting to add python generator

---
+++ docker/data/generate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python -tt
+
+def generate_name():
+  obj = {}
+  obj.first_name = names.get_first_name(gender='female')
+  obj.last_name = names.get_last_name()
+
+def generate_date():
+  
+
+def main():
+  
+
+if __name__ == __main__():
+  main()
