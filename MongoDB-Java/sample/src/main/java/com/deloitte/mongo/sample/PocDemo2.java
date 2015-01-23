9308401402449803753a06d90ffe486213b0d9f5
prakashbhat <prabhat@deloitte.com>
2015-01-23 14:05:33 -0500

    [SNPPROT-36]Sample query for query 2 has been refactored to use converter class to convert from DBObject (in JSON format) to Encounter object (Java object).

--- MongoDB-Java/sample/src/main/java/com/deloitte/mongo/sample/PocDemo2.java
+++ MongoDB-Java/sample/src/main/java/com/deloitte/mongo/sample/PocDemo2.java
@@ -9,18 +9,24 @@ import java.util.Set;
 
 import com.deloitte.mongo.domain.Encounter;
 import com.deloitte.mongo.domain.Observation;
+import com.deloitte.mongo.domain.converters.EncounterReadConverter;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PocDemo2 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PocDemo2.class);
+    
+    @Autowired
+    EncounterReadConverter encounterReadConverter;
 
     public static void main(String[] args) throws Exception {
         PocDemo2 poc = new PocDemo2();
@@ -124,65 +130,6 @@ public class PocDemo2 {
         mongoClient.close();
     }
 
-    //Helper static function
-
-    //Helper static function
-    private static Long doubleToLong(Object aObject) {
-        Double a = (Double) aObject;
-        Double d = (Double) aObject;
-        Long l = (new Double(d)).longValue();
-
-        return l;
-    }
-
-    private static String getFieldDataType(Object aObject, String fieldName) {
-        final String DATA_TYPE_DOUBLE = "Double";
-        final String DATA_TYPE_LONG = "Long";
-        final String DATA_TYPE_INTEGER = "Integer";
-        final String DATA_TYPE_STRING = "String";
-        final String DATA_TYPE_DATE = "Date";
-        final String DATA_TYPE_BSONTimeStamp = "BSONTimeStamp";
-        //Custom data type
-        final String DATA_TYPE_OBSERVATION = "Observation";
-        final String DATA_TYPE_BASICDBLIST = "BasicDBList";
-        final String DATA_TYPE_ARRAYLIST = "ArrayList";  //Supertype of BasicDBList
-
-
-        String objectType = "";
-
-        if (aObject instanceof Double) {
-            objectType = DATA_TYPE_DOUBLE;
-        }
-        if (aObject instanceof Long) {
-            objectType = DATA_TYPE_LONG;
-        }
-        if (aObject instanceof Integer) {
-            objectType = DATA_TYPE_INTEGER;
-        }
-        if (aObject instanceof String) {
-            objectType = DATA_TYPE_STRING;
-        }
-        if (aObject instanceof Date) {
-            objectType = DATA_TYPE_DATE;
-            //Special trace message for date field, just in case
-            System.out.println("Field name " + fieldName + " is of type :" + objectType);
-        }
-        if (aObject instanceof org.bson.types.BSONTimestamp) {
-            objectType = DATA_TYPE_BSONTimeStamp;
-        }
-        if (aObject instanceof ArrayList) {
-            objectType = DATA_TYPE_ARRAYLIST;
-            System.out.println("Field name " + fieldName + " is of type :" + objectType);
-        }
-        if (aObject instanceof com.mongodb.BasicDBList) {
-            objectType = DATA_TYPE_BASICDBLIST;
-        }
-
-        System.out.println("Field name " + fieldName + " is of type :" + objectType);
-
-        return objectType;
-    }
-
     /**
      * Returns the minimum and maximum values from the input observation values.
      * This method is used to obtain the minimum and maximum values from the given observation values.
@@ -212,215 +159,39 @@ public class PocDemo2 {
         return minMaxObservationValue;
     }
 
+    /**
+     * Converts the DBObjects from the result set (DBCursor) into list of Encounter objects.
+     * Note that to convert from JSON DBObject to Encounter POJO, autowired encounterReadConverter class is used.
+     * @param cursor
+     * @return
+     */
     private List<Encounter> convertDBObjectToJavaEncounterObj(DBCursor cursor) {
 
         List<Encounter> encounterList = new ArrayList<Encounter>();
 
         try {
             while (cursor.hasNext()) {
-                //System.out.println(cursor.next());
                 DBObject dbObject = cursor.next();
                 //Convert to POJO and add it to the encounterList
-                System.out.println("encounterJsonStr =" + dbObject);
-                //Convert into POJO
-                //Encounter encounter = jsonToObject(encounterJsonStr, Encounter.class);
-                //encounterList.add(encounter);
-
-                //Display the record
-                //System.out.println(dbObject);
-                System.out.println("Keyset: " + dbObject.keySet());
-                //Keyset: [_id, patient_id, date, type, reason_for_visit, observations]
-				/*
-            	for(String key: dbObject.keySet()) { 
-            		//System.out.print(key+":" + (Long) dbObject.get(key) + "," ); //Casting error from Double to Long
-            		System.out.print(key+":" + doubleToLong(dbObject.get(key)) + "," );
-
-            	}
-            	System.out.println("");
-				 */
-                Set<String> keySet = dbObject.keySet();
-                String[] keyArray = keySet.toArray(new String[0]);
-
-                //Arrays.asList(keyArray)
-                //String[] arr = list.toArray(new String[list.size()]);
-                //To convert from array and later list to set
-				/*
-            	Arrays.asList() would do the trick here.
-            	String[] words = {"ace", "boom", "crew", "dog", "eon"};   
-
-            	List<String> wordList = Arrays.asList(words);  
-            	 For converting to Set, you can do as below
-            	Set<T> mySet = new HashSet<T>(Arrays.asList(words)); 
-				 */
-
-
-                //Populate the POJO by iterationg over the key names
-                Encounter encounter = new Encounter();
-                //IssuedDate issuedDate = new IssuedDate(); //Not needed
-                //Observation observation = new Observation();
-
-                for (String key : keyArray) {
-                    //Java 6 can not switch on String (while Java 7 can). So use if-else
-                    //Just to know get the field data type
-                    //String fieldDataType = getFieldDataType(dbObject.get(key), key);
-
-                    if (key.equalsIgnoreCase("_id")) {
-
-                        encounter.setId(doubleToLong(dbObject.get(key)));
-                        System.out.println("_id:" + encounter.getId());
-                    }
-					/* patient_id is found to be of type Double
-            		if (key.equalsIgnoreCase("patient_id")) {
-            			String patient_id=(String) dbObject.get(key);
-            			encounter.setPatient_id(patient_id) ;
-            			System.out.println("patient_id: " + encounter.getPatient_id());
-            		}
-					 */
-                    if (key.equalsIgnoreCase("patient_id")) {
-                        Double patient_id = (Double) dbObject.get(key);
-
-                        //encounter.setPatient(doubleToLong(patient_id));
-                        //System.out.println("patient_id: " + encounter.getPatientId());
-                    }
-                    //date field is found to be of type String in localhost MongoDB and of type Date in Docker VM MongoDB
-					/*
-					if (key.equalsIgnoreCase("date")) {
-						String date=(String) dbObject.get(key);
-						encounter.setDate(date);
-						System.out.println("date: " + encounter.getDate());
-					}
-					*/
-
-
-                    if (key.equalsIgnoreCase("date")) {
-                        Date date = (Date) dbObject.get(key);
-                        encounter.setDate(date);
-                        System.out.println("date: " + encounter.getDate());
-                    }
-					
-					
-					/*
-            		if (key.equalsIgnoreCase("type")) {
-            			String type=(String) dbObject.get(key);
-            			encounter.setType(type);
-            			System.out.println("type: " + encounter.getType());
-            		}
-					 */
-                    if (key.equalsIgnoreCase("type")) {
-                        Double type = (Double) dbObject.get(key);
-                        //TODO had to comment this out due to conversion issues in new domain design
-                        // encounter.setType(doubleToLong(type));
-                        System.out.println("type: " + encounter.getType());
-                    }
-                    if (key.equalsIgnoreCase("reason_for_visit")) {
-                        String reason_for_visit = (String) dbObject.get(key);
-                        encounter.setReasonForVisit(reason_for_visit);
-                        System.out.println("reason_for_visit: " + encounter.getReasonForVisit());
-                    }
-					/*
-            		if (key.equalsIgnoreCase("observations")) {
-            			Observation[] observations =(Observation[]) dbObject.get(key);
-            			encounter.setObservations(observations);
-            			System.out.println("observations: " + encounter.getObservations());
-            		}
-					 */
-                    if (key.equalsIgnoreCase("observations")) {
-                        List<DBObject> observationObjectList = (List<DBObject>) dbObject.get(key);
-                        DBObject[] observationObjectArray = observationObjectList.toArray(new DBObject[0]);
-
-                        List<Observation> observationList = new ArrayList<Observation>(); // observationList instance
-
-                        for (DBObject dbobjectObsrvn : observationObjectArray) {
-                            System.out.println("Keyset: " + dbobjectObsrvn.keySet());
-                            //Keyset: [name, name_type, value, value_type, applies, issued, identifier, subject]
-                            Set<String> keySetObsrvn = dbobjectObsrvn.keySet();
-                            String[] keyArrayObsrvn = keySetObsrvn.toArray(new String[0]);
-
-                            Observation observation = new Observation();  //New Observation object instance
-
-                            for (String keyObsrvn : keyArrayObsrvn) {
-                                //Java 6 can not switch on String (while Java 7 can). So use if-else
-                                //Just to know get the field data type
-
-                                String fieldDataTypeObsrvn = getFieldDataType(dbobjectObsrvn.get(keyObsrvn), keyObsrvn);
-
-                                if (keyObsrvn.equalsIgnoreCase("name")) {
-
-                                    observation.setName(doubleToLong(dbobjectObsrvn.get(keyObsrvn)));
-                                    System.out.println("name:" + observation.getName());
-                                }
-                                if (keyObsrvn.equalsIgnoreCase("name_type")) {
-
-                                    //TODO had to comment this out due to conversion issues in new domain design
-                                    // observation.setNameType(doubleToLong(dbobjectObsrvn.get(keyObsrvn)));
-                                    System.out.println("name_type:" + observation.getNameType());
-                                }
-                                if (keyObsrvn.equalsIgnoreCase("value")) {
-
-                                    observation.setValue(doubleToLong(dbobjectObsrvn.get(keyObsrvn)));
-                                    System.out.println("value:" + observation.getValue());
-                                }
-                                if (keyObsrvn.equalsIgnoreCase("value_type")) {
-
-                                    //TODO had to comment this out due to conversion issues in new domain design
-                                    // observation.setValueType(doubleToLong(dbobjectObsrvn.get(keyObsrvn)));
-                                    System.out.println("value_type:" + observation.getValueType());
-                                }
-                                if (keyObsrvn.equalsIgnoreCase("applies")) {
-                                    String applies = (String) dbobjectObsrvn.get(keyObsrvn);
-                                    observation.setApplies(applies);
-                                    System.out.println("applies: " + observation.getApplies());
-                                }
-                                //issued
-								/*
-								if (keyObsrvn.equalsIgnoreCase("issued")) {
-									String issued=(String) dbobjectObsrvn.get(keyObsrvn);
-									observation.setIssued(issued); //issued date information
-									System.out.println("issued: " + observation.getIssued());
-								}
-								*/
-                                if (keyObsrvn.equalsIgnoreCase("issued")) {
-                                    Date issued = (Date) dbobjectObsrvn.get(keyObsrvn);
-                                    observation.setIssued(issued); //issued date information
-                                    System.out.println("issued: " + observation.getIssued());
-                                }
-                                if (keyObsrvn.equalsIgnoreCase("identifier")) {
-                                    String identifier = (String) dbobjectObsrvn.get(keyObsrvn);
-                                    observation.setIdentifier(identifier);
-                                    System.out.println("identifier: " + observation.getIdentifier());
-                                }
-                                if (keyObsrvn.equalsIgnoreCase("subject")) {
-                                    String subject = (String) dbobjectObsrvn.get(keyObsrvn);
-                                    observation.setSubject(subject);
-                                    System.out.println("subject: " + observation.getSubject());
-                                }
-                            } //keyset for dbobjectObsrvn iterated.
-                            //Observation object instance populated. Add it to the list now
-                            observationList.add(observation);
-                        } //observationObjectArray iterated
-                        //observationList object instance populated. Convert it to array and assign to encounter.setObservations()
-                        encounter.setObservations(observationList);
-                        //Display
-                        System.out.println("observations: " + encounter.getObservations());
-                        //System.out.println("observations: " + observationList);
-                    }//"observations" field dealt with
-                    //encounterList.add(encounter);
-
-                }
+                LOGGER.debug("encounterJsonStr =" + dbObject);
+                
+                // Use the converter class's convert() method to create POJO. 
+                //AutoWired encounterReadConverter object
+                //EncounterReadConverter encounterReadConverter = new EncounterReadConverter();
+                Encounter encounter = encounterReadConverter.convert(dbObject);
+                
                 encounterList.add(encounter);
                 //Display encounter list
-                //System.out.println("encounterList size=" + encounterList.size() + ", encounterList=" + encounterList);
+                //LOGGER.debug("encounterList size=" + encounterList.size() + ", encounterList=" + encounterList);
             }
             //Display encounter list
-            System.out.println("encounterList size=" + encounterList.size() + ", encounterList=" + encounterList);
+            LOGGER.debug("encounterList size=" + encounterList.size() + ", encounterList=" + encounterList);
 
         } finally {
             cursor.close();
         }
-
         //Return
         return encounterList;
-
     }
 
 }
