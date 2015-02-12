924d5ef871ff26bf8ea41c72beb90f8a76da03c0
Justin Grant <jlgrock@hotmail.com>
2015-02-12 17:15:22 -0500

    [SNPPROT-26] adding PCE Controller and updating PatientController Logger

--- snp/web/src/main/java/com/github/jlgrock/snp/web/controllers/PCEController.java
+++ snp/web/src/main/java/com/github/jlgrock/snp/web/controllers/PCEController.java
@@ -1,7 +1,7 @@
 package com.github.jlgrock.snp.web.controllers;
 
-import com.github.jlgrock.snp.core.data.PatientRepository;
-import com.github.jlgrock.snp.core.domain.Patient;
+import com.github.jlgrock.snp.core.data.PCERepository;
+import com.github.jlgrock.snp.core.domain.PCE;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +11,17 @@ import org.springframework.web.bind.annotation.*;
  *
  */
 @RestController
-@RequestMapping("/patient")
-public class PatientController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PatientController.class);
+@RequestMapping("/pce")
+public class PCEController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PCEController.class);
 
     @Autowired
-    private PatientRepository repository;
+    private PCERepository repository;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public
     @ResponseBody
-    Patient getPatient(@PathVariable final Long id) {
+    PCE getPCE(@PathVariable final Long id) {
         LOGGER.debug("getting encounter");
         return repository.findOne(id);
     }
