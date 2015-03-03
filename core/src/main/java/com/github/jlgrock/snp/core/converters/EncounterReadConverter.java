35a78aa723bf507a44e23f3cf381dd0143cb8e14
Justin Grant <jlgrock@hotmail.com>
2015-03-03 15:07:37 -0500

    [SNPPROT-37] Found another bad artifact

--- core/src/main/java/com/github/jlgrock/snp/core/converters/EncounterReadConverter.java
+++ core/src/main/java/com/github/jlgrock/snp/core/converters/EncounterReadConverter.java
@@ -21,11 +21,8 @@ public class EncounterReadConverter implements Converter<DBObject, Encounter> {
 
     private final ObservationReadConverter observationReadConverter;
 
-    private final PatientRepository patientRepository;
     /**
-     * 
      * @param observationReadConverterIn object of type ObservationReadConverter that has values from a MongoDB object stored in an Observation object.
-     * @param patientRepositoryIn object object of type PatientRepository that holds a patient list.
      */
     @Autowired
     public EncounterReadConverter(final ObservationReadConverter observationReadConverterIn) {
