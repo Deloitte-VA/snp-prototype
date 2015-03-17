94cdcbe1c810521b5a5e188f755e119eb86adab1
Justin Grant <jlgrock@hotmail.com>
2015-03-17 13:51:19 -0400

    [SNPPROT-75] adjusting to make the unit testing work per class on an in-memory implementation

--- web/src/test/java/com/github/jlgrock/snp/web/controllers/EncounterControllerTest.java
+++ web/src/test/java/com/github/jlgrock/snp/web/controllers/EncounterControllerTest.java
@@ -22,6 +22,11 @@ public class EncounterControllerTest extends JerseyTest {
     }
 
     @Test
+    public void testHello() {
+        final String response = target("encounter").request().get(String.class);
+        assertEquals("Hello", response);
+    }
+    @Test
     public void testFindById() {
         final Long id = 1l, patientId = 2l;
         final int type = 9;
