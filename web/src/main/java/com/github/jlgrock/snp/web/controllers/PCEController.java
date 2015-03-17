29df1db88c0bc24b7f79a8e764b4619d45694bb1
shalewis <shalewis@deloitte.com>
2015-03-17 11:13:44 -0400

    Updates to use Jersey

--- web/src/main/java/com/github/jlgrock/snp/web/controllers/PCEController.java
+++ web/src/main/java/com/github/jlgrock/snp/web/controllers/PCEController.java
@@ -2,21 +2,28 @@ package com.github.jlgrock.snp.web.controllers;
 
 import com.github.jlgrock.snp.core.data.PCERepository;
 import com.github.jlgrock.snp.core.domain.PCE;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
+
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 /**
  * The Controller serving up domain objects for PCE objects.
  */
-@RestController
-@RequestMapping("/pce")
+//@RestController
+//@RequestMapping("/pce")
+@Path("/pce")
 public class PCEController {
     private static final Logger LOGGER = LoggerFactory.getLogger(PCEController.class);
 
@@ -34,9 +41,11 @@ public class PCEController {
      * @param id the id of the PCE to get
      * @return the PCE to return to the client
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public PCE getPCE(@PathVariable final Long id) {
+    @GET @Path("/{id}")
+    @Produces("application/json")
+//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//    @ResponseBody
+    public PCE getPCE(@PathParam("id") final Long id) {
         LOGGER.debug("getting encounter");
         return repository.findOne(id);
     }
--- web/src/main/java/com/github/jlgrock/snp/web/controllers/PatientController.java
+++ web/src/main/java/com/github/jlgrock/snp/web/controllers/PatientController.java
@@ -2,23 +2,28 @@ package com.github.jlgrock.snp.web.controllers;
 
 import com.github.jlgrock.snp.core.data.PatientRepository;
 import com.github.jlgrock.snp.core.domain.Patient;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 /**
  * The Controller serving up domain objects for Patient objectsØ.
  */
 
-@RestController
-@RequestMapping("/patient")
+//@RestController
+//@RequestMapping("/patient")
+@Path("/patient")
 public class PatientController {
     private static final Logger LOGGER = LoggerFactory.getLogger(PatientController.class);
 
@@ -33,9 +38,10 @@ public class PatientController {
      * @param id used to find encounter
      * @return single encounter that corresponds to id
      */
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public Patient getPatient(@PathVariable final Long id) {
+    @GET @Path("/{id}")
+//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+//    @ResponseBody
+    public Patient getPatient(@PathParam("id") final Long id) {
         LOGGER.debug("getting encounter");
         return repository.findOne(id);
     }
