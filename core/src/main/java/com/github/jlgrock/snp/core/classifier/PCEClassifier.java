f6e87572cc6291db927aec329e3197dc1b02a55d
rodjohnson <rodjohnson@deloitte.com>
2015-03-27 09:41:16 -0400

    implemented transform method

--- core/src/main/java/com/github/jlgrock/snp/core/classifier/PCEClassifier.java
+++ core/src/main/java/com/github/jlgrock/snp/core/classifier/PCEClassifier.java
@@ -2,6 +2,8 @@ package com.github.jlgrock.snp.core.classifier;
 
 import gov.vha.isaac.logic.LogicGraph;
 
+import com.github.jlgrock.snp.core.domain.ClassifiedAssertion;
+
 /**
  * Replace Post Coordinated Expressions in Logic Graph with Classifier ID
  *
@@ -12,6 +14,12 @@ public class PCEClassifier implements Classifier {
 	public String classify(final LogicGraph logicGraph) {
 		// TODO call a service that takes a LogicGraph and returns a classifier ID
 		return null;
+	}
+	
+	public ClassifiedAssertion transform(String classifierID) {
+		ClassifiedAssertion  classifiedAssertion = new ClassifiedAssertion();
+		classifiedAssertion.setUuid(classifierID);
+		return classifiedAssertion;
 	}
 	
 }
--- core/src/main/java/com/github/jlgrock/snp/core/domain/ClassifiedAssertion.java
+++ core/src/main/java/com/github/jlgrock/snp/core/domain/ClassifiedAssertion.java
@@ -4,4 +4,14 @@ package com.github.jlgrock.snp.core.domain;
  * Marker class for noting that the assertion PCEs have been replaced by classifier ids
  */
 public class ClassifiedAssertion extends Assertion {
+	private String uuid;
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(final String uuidIn) {
+		uuid = uuidIn;
+	}
+	
 }
