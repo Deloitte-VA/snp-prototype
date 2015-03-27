bdd842dfc491ba2f3756de5634239e744bbcf65b
Justin Grant <jlgrock@hotmail.com>
2015-03-27 12:36:50 -0400

    [SNPPROT-86] adding spot for Prakash's code

--- core/src/main/java/com/github/jlgrock/snp/core/classifier/LogicGraphClassifierImpl.java
+++ core/src/main/java/com/github/jlgrock/snp/core/classifier/LogicGraphClassifierImpl.java
@@ -1,8 +1,7 @@
 package com.github.jlgrock.snp.core.classifier;
 
-import com.github.jlgrock.snp.core.domain.ClassifiedAssertion;
+import com.github.jlgrock.snp.core.data.LegoLogicGraphBuilder;
 import gov.vha.isaac.logic.LogicGraph;
-import gov.vha.isaac.logic.LogicGraphBuilder;
 
 import java.util.UUID;
 
@@ -12,20 +11,24 @@ import java.util.UUID;
  */
 public class LogicGraphClassifierImpl implements LogicGraphClassifier {
 
-	private LogicGraphBuilder logicGraphBuilder;
+	private LegoLogicGraphBuilder legoLogicGraphBuilder;
 
-	public LogicGraphClassifierImpl(final LogicGraphBuilder logicGraphBuilderIn) {
-		logicGraphBuilder = logicGraphBuilderIn;
+	public LogicGraphClassifierImpl(final LegoLogicGraphBuilder legoLogicGraphBuilderIn) {
+		legoLogicGraphBuilder = legoLogicGraphBuilderIn;
 	}
 
 	@Override
 	public UUID classify(final LogicGraph logicGraph) {
 		String classifierID = "";
-		// TODO : call a service that takes a LogicGraph and returns a classifier ID
-		//classifierID = vaLogicService.classify(logicGraph);
 
-		logicGraphBuilder.create();
-		return transform(classifierID);
+		//Set the expression from the
+		legoLogicGraphBuilder.setExpression(expression);
+		legoLogicGraphBuilder.create();
+		legoLogicGraphBuilder.getLogicGraph();
+
+		//TODO more here for Shane
+
+		return null;
 	}
 	
 }
--- core/src/main/java/com/github/jlgrock/snp/core/data/LegoLogicGraphBuilder.java
+++ core/src/main/java/com/github/jlgrock/snp/core/data/LegoLogicGraphBuilder.java
@@ -1,6 +1,7 @@
 package com.github.jlgrock.snp.core.data;
 
 import com.github.jlgrock.snp.core.model.parser.Expression;
+import gov.vha.isaac.logic.LogicGraph;
 import gov.vha.isaac.logic.LogicGraphBuilder;
 
 /**
@@ -9,17 +10,22 @@ import gov.vha.isaac.logic.LogicGraphBuilder;
 public class LegoLogicGraphBuilder extends LogicGraphBuilder {
 
     private Expression expression;
+    private LogicGraph logicGraph;
 
     /**
      * Set the expression to build the logic graph
      * @param expressionIn the expression to create a logic graph from
      */
-    void setExpression(final Expression expressionIn) {
+    public void setExpression(final Expression expressionIn) {
         expression = expressionIn;
     }
 
     @Override
     public void create() {
         //TODO Prakash's code goes here
+    }
+
+    public LogicGraph getLogicGraph() {
+        return logicGraph;
     }
 }
