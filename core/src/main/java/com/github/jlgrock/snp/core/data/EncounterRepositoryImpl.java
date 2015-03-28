851dba6987a162b59da8d8b38dbea2c58f73ff3f
vbhole <vbhole@deloitte.com>
2015-03-28 17:27:29 -0400

    More changes.

--- core/src/main/java/com/github/jlgrock/snp/core/data/EncounterRepositoryImpl.java
+++ core/src/main/java/com/github/jlgrock/snp/core/data/EncounterRepositoryImpl.java
@@ -17,23 +17,23 @@ import java.util.List;
 /**
  *
  */
-public class EncounterRepositoryImpl extends AbstractRepositoryImpl<Encounter, Long> implements EncounterRepository {
+public class EncounterRepositoryImpl extends
+		AbstractRepositoryImpl<Encounter, Long> implements EncounterRepository {
 
-    protected EncounterRepositoryImpl(MongoDbFactory mongoDbFactoryIn) {
+	protected EncounterRepositoryImpl(MongoDbFactory mongoDbFactoryIn) {
 		super(mongoDbFactoryIn);
 	}
 
 	@Override
-    public List<Encounter> findByDate(Date date) {
-		
-			List<Encounter> eList = new ArrayList<Encounter>();
-			DBCollection dbc1 = dBCollection();
-			BasicDBObject query = new BasicDBObject ("Encounter.date", date); 
-			DBCursor x = dbc1.find(query);
-	        for(Encounter o:x){
-	        	eList.add(o);	
-	        }
-			return eList;
-    }
+	public List<Encounter> findByDate(Date date) {
+		List<Encounter> eList = new ArrayList<Encounter>();
+		DBCollection dbc1 = dBCollection();
+		BasicDBObject query = new BasicDBObject("Encounter.date", date);
+		DBCursor x = dbc1.find(query);
+		for (Encounter o : x) {
+			eList.add(o);
+		}
+		return eList;
+	}
 
 }
