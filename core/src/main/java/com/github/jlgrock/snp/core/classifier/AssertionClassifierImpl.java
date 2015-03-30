af78b959ecfcb1a447f07075bfcb55c008e7f6c5
rodjohnson <rodjohnson@deloitte.com>
2015-03-30 14:55:21 -0400

    classify complex expressions only

--- core/src/main/java/com/github/jlgrock/snp/core/classifier/AssertionClassifierImpl.java
+++ core/src/main/java/com/github/jlgrock/snp/core/classifier/AssertionClassifierImpl.java
@@ -1,13 +1,10 @@
 package com.github.jlgrock.snp.core.classifier;
 
-import gov.vha.isaac.logic.LogicGraph;
-
 import java.io.IOException;
 import java.util.UUID;
 
 import org.ihtsdo.otf.tcc.api.contradiction.ContradictionException;
 
-import com.github.jlgrock.snp.core.data.LegoLogicGraphBuilder;
 import com.github.jlgrock.snp.core.domain.ClassifiedAssertion;
 import com.github.jlgrock.snp.core.model.parser.Assertion;
 import com.github.jlgrock.snp.core.model.parser.Expression;
@@ -16,21 +13,16 @@ public class AssertionClassifierImpl implements AssertionClassifier {
 
 	@Override
 	public ClassifiedAssertion classify(Assertion assertion) throws IOException, ContradictionException {
-		LogicGraph lg = buildLogicGraph(assertion);		
-		LegoLogicGraphBuilder lgBuilder = new LegoLogicGraphBuilder(0, null, null);
+		LogicGraphClassifierImpl lgClassifier = new LogicGraphClassifierImpl();
 		Expression expression = assertion.getDiscernible().getExpression();
-		lgBuilder.setExpression(expression);
-		LogicGraphClassifierImpl lgClassifier = new LogicGraphClassifierImpl(lgBuilder);
-		UUID uuid = lgClassifier.classify(expression);
 		ClassifiedAssertion classifiedAssertion = new ClassifiedAssertion();
-		classifiedAssertion.setUuid(uuid);
+		//check for complex expression since simple expression is not being classified
+		if(expression.getRelations().size() > 0) {
+			UUID uuid = lgClassifier.classify(expression);
+			classifiedAssertion.setUuid(uuid);
+		}
+		
 		return classifiedAssertion;
 	}
 
-	public LogicGraph buildLogicGraph(Assertion assertion) throws IOException, ContradictionException {
-		Expression expression = assertion.getDiscernible().getExpression();
-		//TODO : sourceConceptSctid, isAboutUUID, destinationUUID
-		LegoLogicGraphBuilder lgBuilder = new LegoLogicGraphBuilder(0, null, null);
-		return lgBuilder.getLogicGraph();
-	}
 }
