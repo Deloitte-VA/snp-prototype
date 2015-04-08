b208d33a474d49b9ec9299407cf5090c2b025d0e
rodjohnson <rodjohnson@deloitte.com>
2015-04-08 10:53:52 -0400

    added java docs

--- core/src/main/java/com/github/jlgrock/snp/core/parser/XmlParser.java
+++ core/src/main/java/com/github/jlgrock/snp/core/parser/XmlParser.java
@@ -4,5 +4,10 @@ import java.io.InputStream;
 
 public interface XmlParser<T> {
 
+	/**
+     * Parse the XML input stream into java objects
+     * @param xmlInput the input stream
+     * @return the deserialized java objects
+     */
 	T parseDocument(final InputStream xmlInput);
 }
