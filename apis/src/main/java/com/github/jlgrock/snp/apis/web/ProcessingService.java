89e197a41a3893faa7806a14f4cb1aad1805e32b
Justin Grant <jlgrock@hotmail.com>
2015-05-28 14:46:31 -0400

    [SNPPROT-98] adding @Contract to some of the interfaces

--- apis/src/main/java/com/github/jlgrock/snp/apis/web/ProcessingService.java
+++ apis/src/main/java/com/github/jlgrock/snp/apis/web/ProcessingService.java
@@ -1,8 +1,11 @@
 package com.github.jlgrock.snp.apis.web;
 
+import org.jvnet.hk2.annotations.Contract;
+
 /**
  *
  */
+@Contract
 public interface ProcessingService<T> {
     void processInput(final String input);
 
