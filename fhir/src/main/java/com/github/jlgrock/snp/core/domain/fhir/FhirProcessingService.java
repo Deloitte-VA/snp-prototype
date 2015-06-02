79156dde9eb6ffd5ce46b079f72f11d7e665e271
Justin Grant <jlgrock@hotmail.com>
2015-06-02 14:04:01 -0400

    [SNPPROT-98] trying to fix Dependency injection issues

--- fhir/src/main/java/com/github/jlgrock/snp/core/domain/fhir/FhirProcessingService.java
+++ fhir/src/main/java/com/github/jlgrock/snp/core/domain/fhir/FhirProcessingService.java
@@ -4,9 +4,10 @@ import com.github.jlgrock.snp.apis.exceptions.ClassifierException;
 import com.github.jlgrock.snp.apis.exceptions.UnmarshallingException;
 import com.github.jlgrock.snp.apis.web.MediaTypeService;
 import com.github.jlgrock.snp.apis.web.ProcessingService;
-import com.github.jlgrock.snp.core.domain.fhir.processors.FhirElementProcessorFactory;
-import com.github.jlgrock.snp.core.domain.fhir.processors.FhirElementProcessorService;
 import com.github.jlgrock.snp.core.domain.fhir.marshallers.FhirMarshallerService;
+import com.github.jlgrock.snp.core.domain.fhir.processors.FhirElementProcessorFactory;
+import com.github.jlgrock.snp.core.domain.fhir.processors.FhirElementProcessorFactoryImpl;
+import com.github.jlgrock.snp.core.domain.fhir.processors.FhirElementProcessorService;
 import org.jvnet.hk2.annotations.Service;
 
 import javax.inject.Inject;
@@ -24,7 +25,7 @@ public class FhirProcessingService implements ProcessingService {
     @Inject
     public FhirProcessingService(@Named("FhirMediaTypeService") final MediaTypeService mediaTypeIn,
                           final FhirMarshallerService fhirMarshallerServiceIn,
-                          final FhirElementProcessorFactory fhirElementProcessorFactoryIn) {
+                          final FhirElementProcessorFactoryImpl fhirElementProcessorFactoryIn) {
         mediaType = mediaTypeIn;
         fhirMarshallerService = fhirMarshallerServiceIn;
         fhirElementProcessorFactory = fhirElementProcessorFactoryIn;
