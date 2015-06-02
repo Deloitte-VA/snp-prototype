5771e4232148add9133615780a988b41861a5da9
Justin Grant <jlgrock@hotmail.com>
2015-06-02 15:16:01 -0400

    [SNPPROT-98] trying to fix Dependency injection issues

--- lego/src/main/java/com/github/jlgrock/snp/core/domain/lego/marhsallers/LegoMarshallerService.java
+++ lego/src/main/java/com/github/jlgrock/snp/core/domain/lego/marhsallers/LegoMarshallerService.java
@@ -1,12 +1,14 @@
 package com.github.jlgrock.snp.core.domain.lego.marhsallers;
 
 import com.github.jlgrock.snp.apis.exceptions.UnmarshallingException;
+import org.jvnet.hk2.annotations.Contract;
 
 import java.io.Reader;
 
 /**
  *
  */
+@Contract
 public interface LegoMarshallerService {
     public Object unmarshall(final String input) throws UnmarshallingException;
 
