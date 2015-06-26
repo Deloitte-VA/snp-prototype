731d21eb9c4ca6897b7706e04b04c86f873a774f
rodjohnson <rodjohnson@deloitte.com>
2015-06-26 18:30:43 -0400

    [SNPPROT-112] submit query using RESTful query interface

--- web/src/main/webapp/js/services/DynamicQueryServices.js
+++ web/src/main/webapp/js/services/DynamicQueryServices.js
@@ -3,15 +3,23 @@
 /* Services */
 
 app.factory('dynamicQueryFactory', ['$http', function($http) {
-  var urlBase = 'services/classifier/query';
+  var urlBase = 'services/patient/search';
   var dataFactory = {};
 
   dataFactory.getPatients = function (formData) {
-      return $http.post(urlBase,
+      //return $http.post(urlBase,
     		  //$.param(formData),  // pass in data as strings
-    		  formData//,
+    		  //formData,
     		  //{headers:{'Content-Type': 'application/x-www-form-urlencoded'}}  // set the headers so angular passing info as form data (not request payload)
-      );
+      //);
+      return $http.get(urlBase, {
+          params: {
+        	  observation: formData.observation,
+        	  provenance: formData.category,
+        	  timing: formData.timing,
+        	  value: formData.value
+          }
+       })
   };  
   
   return dataFactory;
