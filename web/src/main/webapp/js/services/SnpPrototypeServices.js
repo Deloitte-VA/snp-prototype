03a372a986ad9d8dff47d32f9e9e541edbf77d4a
rodjohnson <rodjohnson@deloitte.com>
2015-06-29 01:32:39 -0400

    [SNPPROT-112] move queryForm.html to index.html and removed old js files

--- web/src/main/webapp/js/services/SnpPrototypeServices.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Services */
-
-var snpPrototypeServices = angular.module('SnpPrototypeServices', ['ngResource']);
-
-snpPrototypeServices.service('SnpPrototype', ['$resource', function($resource) { 
-	return $resource('patients/patients.json');
-	}]);
