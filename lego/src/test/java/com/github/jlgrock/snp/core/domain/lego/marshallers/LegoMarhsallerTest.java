d2fe1460b56993d0b9f97b9bb2f060a0c09998df
Justin Grant <jlgrock@gmail.com>
2015-07-07 12:24:52 -0400

    [SNPPROT-122] writing test cases...

--- lego/src/test/java/com/github/jlgrock/snp/core/domain/lego/marshallers/LegoMarhsallerTest.java
+++ lego/src/test/java/com/github/jlgrock/snp/core/domain/lego/marshallers/LegoMarhsallerTest.java
@@ -1,30 +1,22 @@
-package com.github.jlgrock.snp.core.domain.lego;
+package com.github.jlgrock.snp.core.domain.lego.marshallers;
 
+import com.github.jlgrock.snp.core.domain.lego.AbstractClassificationTest;
 import com.github.jlgrock.snp.core.domain.lego.model.Lego;
 import com.github.jlgrock.snp.core.domain.lego.model.LegoList;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.Reader;
 
 /**
  *
  */
-public class LegoTopLevelTest extends AbstractClassificationTest {
-
+public class LegoMarhsallerTest extends AbstractClassificationTest {
     @Test
-    public void testLegoSerialization() throws Exception {
-        JAXBContext jc = JAXBContext.newInstance(path);
-        Unmarshaller unmarshaller = jc.createUnmarshaller();
-        Reader reader = readFile("lego.xml");
-        Object o = unmarshaller.unmarshal(reader);
-        Assert.assertTrue(o instanceof Lego);
-    }
-
-    @Test
-    public void testLegoListSerialization() throws Exception {
+    void unmarshallLegoList() throws JAXBException {
         JAXBContext jc = JAXBContext.newInstance(path);
         Unmarshaller unmarshaller = jc.createUnmarshaller();
         Reader reader = readFile("legolist.xml");
@@ -32,6 +24,13 @@ public class LegoTopLevelTest extends AbstractClassificationTest {
         Assert.assertTrue(o instanceof LegoList);
     }
 
-
+    @Test
+    void unmarshallLego() throws JAXBException {
+        JAXBContext jc = JAXBContext.newInstance(path);
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        Reader reader = readFile("lego.xml");
+        Object o = unmarshaller.unmarshal(reader);
+        Assert.assertTrue(o instanceof Lego);
+    }
 
 }
