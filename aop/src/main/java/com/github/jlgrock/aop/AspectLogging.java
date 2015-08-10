3b0f3f0a74a1f2f8a69035b0fea6032dfce38192
Kun Yue <ykssky1@gmail.com>
2015-08-10 16:21:52 -0400

    [SNPPROT-130] excluded primitives package for avoiding error -- illegal modifiers: 0x18

--- aop/src/main/java/com/github/jlgrock/aop/AspectLogging.java
+++ aop/src/main/java/com/github/jlgrock/aop/AspectLogging.java
@@ -13,79 +13,58 @@ import org.aspectj.lang.reflect.CodeSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Method;
-
 @Aspect
 public class AspectLogging {
     private static final Logger aopLogger = LoggerFactory.getLogger("aopLogger");
 
-//    @Pointcut("execution(* com.github.jlgrock.snp.web.configuration.*.*(..))")//good
-//    @Pointcut("execution(* com.github.jlgrock.snp.web.configuration..*(..)) && execution(!com.github.jlgrock.snp.web.configuration.package-info.*(..))")
-//    @Pointcut("within(com.github.jlgrock.snp.web.configuration.ApplicationConfig)")
-//    @Pointcut("within(com.github.jlgrock.snp.web..*)")
-//    @Pointcut("execution(* com.github.jlgrock.snp.web.zzsandbox.TargetClass.doSomething(..))")
-
     @Pointcut("execution(* com.github.jlgrock.snp..*.*(..))")
-//    @Pointcut("execution(* main(..))")//good
-    public void logging() {}
+    public void everything() {}
+
+    @Pointcut("execution(* com.github.jlgrock.snp.domain.types..*.*(..))")
+    public void notLogging() {}
+
+    @Pointcut("everything() && ! notLogging()")
+    public void logging(){}
 
     @Around("logging()")
     public Object logging(ProceedingJoinPoint pjp) throws Throwable {
-        aopLogger.info("\n******************************************************\n");
+        aopLogger.info("\r\n******************************************************\r\n");
         aopLogger.info("Before " + pjp.getSignature());
         aopLogger.info(getLogInfo(pjp));
 
         Object ret = pjp.proceed();
 
-        aopLogger.info("\n******************************************************\n");
+        aopLogger.info("\r\n******************************************************\r\n");
         aopLogger.info("After " + pjp.getSignature());
-
-
 
         return ret;
     }
 
+    /**
+     * get logging info for class, method, parameter names & values.
+     * @param joinPoint
+     * @return
+     */
     private String getLogInfo(JoinPoint joinPoint) {
         StringBuilder builder = new StringBuilder();
-
-        Object o = joinPoint.getThis();
-        if (o != null) {
-            builder.append("Class name *************** : ");
-            builder.append(
-                o.getClass()
-                 .getName()
-            );
-            builder.append("\n");
-
-        }
-
-        builder.append("signature &&& : ");
-        builder.append(
-            joinPoint.getSignature()
-                     .toLongString()
-        );
-        builder.append("\n");
-
 
         Object[] signatureArgs = joinPoint.getArgs();
         Class[] parameterTypes = ((CodeSignature) joinPoint.getSignature()).getParameterTypes();
         String[] parameterNames = ((CodeSignature) joinPoint.getSignature()).getParameterNames();
 
-        builder.append("\n******************************************************\n");
+        builder.append("\r\n******************************************************\r\n");
         for (int i = 0; i < joinPoint.getArgs().length; i++) {
-            builder.append(String.format("Arg %d :\n", i));
+            builder.append(String.format("Arg %d :\r\n", i));
             builder.append("type: ");
             builder.append(parameterTypes[i]);
-            builder.append("\n");
+            builder.append("\r\n");
             builder.append("name: ");
             builder.append(parameterNames[i]);
-            builder.append("\n");
+            builder.append("\r\n");
             builder.append("value: ");
             builder.append(signatureArgs[i]);
-            builder.append("\n");
+            builder.append("\r\n\r\n");
         }
-
-
 
         return builder.toString();
     }
